8b7b4a637b47abb55d2512ec5db07c376eab5c07
CryptoNeverSleeps <36324479+CryptoNeverSleeps@users.noreply.github.com>
2018-06-03 07:49:30 -0400

    Update abs_install.sh

--- abs_install.sh
+++ abs_install.sh
@@ -95,7 +95,7 @@ if [ "$?" -gt "0" ];
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
     echo "apt-get update"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake nano jq htop git pwgen curl libdb4.8-dev \
 bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban python-virtualenv unzip"
  exit 1
 fi
